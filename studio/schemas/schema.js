1e21c26b9622f3057ee59ef9182981c7246d254e
Steph Tyszka <steph@nuclio.org>
2019-10-11 19:24:29 +0200

    studio: debug - cut down toast object

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -8,7 +8,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import author from './documents/author'
 import category from './documents/category'
 import post from './documents/post'
-import toast from './documents/project'
+import toast from './documents/toast'
 import siteSettings from './documents/siteSettings'
 
 // Object types
